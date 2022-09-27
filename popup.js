e5392ed6fc6cb9e2e226b5bc3170591eaa4bdb74
Omotayo <omotayo8@live.com>
2022-09-27 05:02:11 +0100

    active chat rooms added

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 let body = document.getElementsByTagName('body');
 let container = document.querySelector('#grid-container');
 let grid1 = document.querySelector('#grid1');
+let grid2 = document.querySelector('#grid2');
 let team = document.querySelector('#team');
 let user_details = document.querySelector('#user_details');
 let prod = document.querySelector('#products');
@@ -11,15 +12,20 @@ let chat  = document.querySelector('#chat');
 let enter = document.querySelector('#enter');
 let list = document.createDocumentFragment();
 
+/*
+chrome.storage.sync.set({key: value}, function() {
+  console.log('Value is set to ' + value);
+});
+
+chrome.storage.sync.get(['key'], function(result) {
+  console.log('Value currently is ' + result.key);
+});
+*/
+
 window.onload = (event) => {
      body[0].style.width = '62px';
      body[0].style.backgroundColor = '#f2f3f4';     
      console.log('PAGE DID LOAD');
-
-
-
-
-
 };
 
 function HideScrollbar() {
@@ -40,13 +46,14 @@ team.addEventListener('click', (event)=>{
  	//let descTag = document.createElement('span');
  	
  	wordo.style.color = '#018749';
- 	wordo.innerHTML = 'Dowell Team';
+ 	wordo.textContent = 'Dowell Team';
     wordo.style.fontFamily = 'Andale Mono, monospace, Courier New, monospace';
  	descLink.src = 'https://ll07-team-dowell.github.io/DowellJobPortal/';
  	//descTag.innerHTML = 'Job Portal';
  	//descLink.appendChild(descTag);
     descLink.height = '700px'; 	
  	descLink.width = '270px';
+    descLink.loading = 'lazy';
 
     divy.appendChild(brk2);
  	divy.appendChild(wordo);
@@ -61,9 +68,9 @@ team.addEventListener('click', (event)=>{
     grid1.style.overflow = 'scroll';
     body[0].style.width = '350px'; 
     body[0].style.backgroundColor='beige';
-    HideScrollbar();
- 	
- }, {once : true});
+    HideScrollbar(); 	
+
+ }, {once : true});   
 
 
 
@@ -73,7 +80,7 @@ prod.addEventListener('click', (event)=>{
     
     let br2 = document.createElement('br');
     let wordo = document.createElement('h2');    
-    wordo.innerHTML = 'Products';
+    wordo.textContent = 'Products';
     let br = document.createElement('br');
     wordo.style.color = '#018749';
     wordo.style.textAlign = 'center';
@@ -92,7 +99,8 @@ prod.addEventListener('click', (event)=>{
     descLink.style.padding = '20px';
     let image = document.createElement('img');
     image.src = "https://img.icons8.com/windows/96/000000/design-portal.png"
-    image.title = 'Dowell Portal';    
+    image.title = 'Dowell Portal';
+    image.loading = 'lazy';    
         
     let descLink1 = document.createElement('a');
     descLink1.href = 'https://100069.pythonanywhere.com';
@@ -102,6 +110,7 @@ prod.addEventListener('click', (event)=>{
     let image1 = document.createElement('img');
     image1.src = 'https://img.icons8.com/doodle/96/000000/messaging-.png';
     image1.title = 'Dowell Chat';
+    image1.loading = 'lazy';
     
     let descLink2 = document.createElement('a');
     descLink2.href = 'https://100035.pythonanywhere.com/nps-admin';
@@ -111,6 +120,7 @@ prod.addEventListener('click', (event)=>{
     let image2 = document.createElement('img');
     image2.src = 'https://img.icons8.com/avantgarde/100/000000/ruler.png';
     image2.title = 'NPS Scale';
+    image2.loading = 'lazy';
 
     let descLink3 = document.createElement('a');
     descLink3.href = 'https://liveuxstoryboard.com/';
@@ -120,6 +130,7 @@ prod.addEventListener('click', (event)=>{
     let image3 = document.createElement('img');
     image3.src = 'https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-storyboard-filmmaking-flaticons-flat-flat-icons.png';
     image3.title = 'Live UX Storyboard';
+    image3.loading = 'lazy';
 
     //wordo.appendChild(br); 
     divy.appendChild(wordo);
@@ -149,15 +160,28 @@ prod.addEventListener('click', (event)=>{
 chat.addEventListener('click', (event)=>{
   event.preventDefault()
   grid1.replaceChildren()
+
   let divy = document.createElement('center');
   let brk2 = document.createElement('br');
   let wordo = document.createElement('h2');
+  let btn = document.createElement('button');
   
   wordo.style.color = '#018749';
-  wordo.innerHTML = 'Chat Rooms';
+  wordo.textContent = 'Chat Rooms';
   wordo.style.fontFamily = 'Andale Mono, monospace, Courier New, monospace';   
+  btn.height = '50px';
+  btn.className = 'chat-btn';
+
+  btn.style.marginTop = '50px';
+  btn.style.marginBottom = '50px';
+  btn.style.marginRight = '50px';
+  btn.style.marginLeft = '50px';
+  btn.style.borderRadius = '15px';
+  btn.textContent = 'Click To See';
+
   divy.appendChild(brk2);
   divy.appendChild(wordo);
+  divy.appendChild(btn);
   list.appendChild(divy);
   grid1.appendChild(list);
 
@@ -165,8 +189,47 @@ chat.addEventListener('click', (event)=>{
   grid2.style.backgroundColor = 'transparent';
   body[0].style.width = '350px';
   body[0].style.backgroundColor='beige';  
-    
+
+  //sessionStorage.setItem("retainState", `${grid1}`); 
+
+  //***************Another Layer***************
+  let chatLayer1 = document.querySelector('.chat-btn');
+  chatLayer1.addEventListener('click', (event)=>{
+                event.preventDefault()
+                grid1.replaceChildren()
+                
+                let url = 'http://100069.pythonanywhere.com/chat/get-rooms/';
+                fetch(url, {
+                      method: 'GET',
+                      headers: {
+                          'Accept': 'application/json, text/plain, */*',
+                          'content-type':'application/json'
+                            },
+                            })
+                .then(res => res.json())
+                .then((data)=>{
+                    window.htmlString = '';                    
+                        data.map(function(room){
+                            //window.apiRooms = room.room_name;
+                            htmlString += `<hr><p></p><div style="margin-bottom: 8px; 
+                                    padding: 8px; width: 256px;
+                                    background-color: lightgray;
+                                    border: 2px solid; 
+                                    border-radius: 4px;
+                                    margin-left: 5px;
+                                    margin-top: 5px;"><a href=''>${room.room_name}</a></div>`;  
+                            
+                            grid1.innerHTML = htmlString;                    
+                            grid1.style.visibility = 'visible';
+                            grid2.style.backgroundColor = 'transparent';
+                            
+                            console.log(room.room_name)                            
+                               })
+                        
+                })
+                .catch(error => {console.log(error)})
+                
+            }, {once : true}); 
+
  }, {once : true});
  
-
-
